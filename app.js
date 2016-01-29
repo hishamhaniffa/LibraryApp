bcd3e634823c2ca1fee8923ec7a37085f1a08761
Hisham Haniffa <hishamhaniffa4@gmail.com>
2016-01-30 00:56:54 +0530

    Add /Books route and books view

--- app.js
+++ app.js
@@ -11,15 +11,60 @@ app.set('views', './src/views');
 //var handlebars = require('express-handlebars');
 //app.engine('.hbs', handlebars({extname: '.hbs'}));
 
+var bookRouter = express.Router();
+
 app.set('view engine', 'ejs'); // for jade and ejs only change the view engine
 
+var books = [
+    {
+        title: 'War and Peace',
+        genre: 'Historical Fiction',
+        author: 'Lev Niklos',
+        read: false
+    },
+    {
+        title: 'Oliver Twist',
+        genre: 'Historical Fiction',
+        author: 'Charles Willim',
+        read: true
+    }
+];
+
+bookRouter.route('/')
+    .get(function (req, res) {
+        res.render('books', {
+            title: 'Hello from render',
+            nav: [{
+                Link: '/Books',
+                Text: 'Books'
+        }, {
+                Link: '/Authors',
+                Text: 'Authors'
+        }],
+            books: books
+        });
+    });
+
+bookRouter.route('/single')
+    .get(function (req, res) {
+        res.send('Hello Single Book');
+    });
+
+app.use('/Books', bookRouter);
+
 app.get('/', function (req, res) {
-    res.render('index', {title:'Hello from render',list: ['a','b']});
+    res.render('index', {
+        title: 'Hello from render',
+        nav: [{
+            Link: '/Books',
+            Text: 'Books'
+        }, {
+            Link: '/Authors',
+            Text: 'Authors'
+        }]
+    });
 });
 
-app.get('/books', function (req, res) {
-    res.send('Hello Books');
-});
 
 app.listen(port, function (err) {
     console.log('Running server on port: ' + port);
