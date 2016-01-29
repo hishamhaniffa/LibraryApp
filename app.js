3294f3e1b4b6864973acd7ce8eba6dc79e153468
Hisham Haniffa <hishamhaniffa4@gmail.com>
2016-01-30 00:30:07 +0530

    Checkout Jade, hbs and ejs template engine

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
+/* jshint node: true */
+
 var express = require('express');
 var app = express();
-var port = 3000;
+var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-   res.send('Hello World!'); 
+app.use(express.static('public'));
+app.set('views', './src/views');
+
+// for handelbars
+//var handlebars = require('express-handlebars');
+//app.engine('.hbs', handlebars({extname: '.hbs'}));
+
+app.set('view engine', 'ejs'); // for jade and ejs only change the view engine
+
+app.get('/', function (req, res) {
+    res.render('index', {title:'Hello from render',list: ['a','b']});
 });
 
-app.get('/books', function(req, res){
-   res.send('Hello Books'); 
+app.get('/books', function (req, res) {
+    res.send('Hello Books');
 });
 
-app.listen(port, function(err){
+app.listen(port, function (err) {
     console.log('Running server on port: ' + port);
 });
